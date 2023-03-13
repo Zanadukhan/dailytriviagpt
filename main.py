d69af02361e65a5330b850445917e82032a8649d
Zanadukhan <cchiu231998@hotmail.ca>
2023-03-12 18:09:52 -0700

    finished textsender class functionality + cleaned up main.py

--- main.py
+++ main.py
@@ -13,36 +13,21 @@ prompt = f'Present a historical fact that happened on {current_date}.' \
          'Start it off with, "On this day, February 2 in *year this fact takes place*"' \
          ' as the header and end with a short paragraph describing the event.'
 
-trivia = TriviaGenerator()
-trivia.set_prompt(prompt)
-trivia.generate_text()
+if __name__ == '__main__':
+    trivia = TriviaGenerator()
+    trivia.set_prompt(prompt)
+    trivia.generate_text()
 
-img = ImgGen(trivia.shortened_trivia)
+    img = ImgGen(trivia.shortened_trivia)
 
-textsender = TextSender()
+    textsender = TextSender()
 
-textsender.send_message(trivia.trivia_text, img.output)
-
-# schedule.every().day.at(send_time).do(message)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    schedule.every().day.at('18:06').do(textsender.send_message, text_input=trivia.trivia_text, img_input=img.output)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
-#
-# account_sid = os.environ['TWILIO_ACCOUNT_SID']
-# auth_token = os.environ['TWILIO_AUTH_TOKEN']
-# client = Client(account_sid, auth_token)
-#
-
-# def send_message():
-#     client.messages \
-#                 .create(
-#                      body=f"{trivia.trivia_text}",
-#                      from_='+15074797617',
-#                      media_url=img.output,
-#                      to='+17788855315'
-#                  )
 
 
 
